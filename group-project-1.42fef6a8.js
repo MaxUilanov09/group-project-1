a5479511a072ffe8202325123f33041753804463
DanyloSuslinec2010 <danickasuslinec@gmail.com>
2025-05-28 17:23:33 +0000

    Deploying to gh-pages from @ MaxUilanov09/group-project-1@525f6c9c84bbb8a1e0bea428ff571c93b29dfeae 🚀

---
+++ group-project-1.42fef6a8.js
@@ -0,0 +1,2 @@
+const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n);
+//# sourceMappingURL=group-project-1.42fef6a8.js.map
